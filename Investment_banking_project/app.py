f07911861e982a55ff5a8308bf463e3e50613674
priyankabhartiya11 <141410737+priyankabhartiya11@users.noreply.github.com>
2023-09-21 12:14:42 +0530

    Add files via upload

---
+++ Investment_banking_project/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, jsonify
+import openai
+
+app = Flask(__name__)
+
+openai.api_key = "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"
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+    prompt = request.form['prompt']
+
+    # Call the OpenAI API with the prompt
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=prompt,
+        max_tokens=150  
+    )
+
+    # Extract and return the OpenAI response
+    reply = response.choices[0].text.strip()
+    return jsonify({"reply": reply})
+
+if __name__ == '__main__':
+    app.run(debug=True)
